2dcd55e62ad99659590c91eb19f476dfb6c1a63c
aliencrack11 <xalienproxmax@gmail.com>
2025-12-19 22:41:44 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1153,8 +1153,9 @@ async def on_command_error(ctx: commands.Context, error: commands.CommandError):
 
 # ---------------- Run ----------------
 if __name__ == "__main__":
-    bot.run()
+    bot.run(TOKEN)
 
 
    
+
 
